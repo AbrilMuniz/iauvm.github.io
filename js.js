566d28db3dbfe2779f10e5df6850565beb14b211
AbrilMuniz <161386239+AbrilMuniz@users.noreply.github.com>
2025-02-18 21:15:19 -0600

    initial commit

---
+++ js.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const animatedElements = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  animatedElements.forEach((element) => observer.observe(element));
+});
